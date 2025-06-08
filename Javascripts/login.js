ff20a2a9fbb8fcc0f52b9f103cbc721797bf2ee0
KAIXEN <kennethjolloso@gmail.com>
2025-06-08 23:14:49 +0800

    Update login.js

--- Javascripts/login.js
+++ Javascripts/login.js
@@ -1,28 +1,23 @@
 document.getElementById('login-form').addEventListener('submit', function(event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    const userType = document.getElementById('user_type').value;
+  const userType = document.getElementById('user_type').value;
 
-    sessionStorage.setItem('user_type', userType);
+  sessionStorage.setItem('user_type', userType);
 
-    if (userType === 'admin') {
-        window.location.href = 'Admin-login.html';
-    } else if (userType === 'finance') {
-        window.location.href = 'Finance-login.html';
+  if (userType === 'admin') {
+    window.location.href = 'admin-login.html';
+  } else {
+    if (userType === 'finance') {
+      window.location.href = 'finance.html';
     } else if (userType === 'buyer') {
-        window.location.href = 'Buyer-login.html';
+      window.location.href = 'buyer.html';
     } else if (userType === 'manager') {
-        window.location.href = 'Manager-login.html';
+      window.location.href = 'approver.html';
     } else if (userType === 'por') {
-        window.location.href = 'PO-PR-login.html';
+      window.location.href = 'requester.html';
+    } else {
+      alert('Unknown department selected');
     }
-
-    if (userType === 'admin') {
-        // Placeholder for admin login logic
-        if (username === 'admin' && password === 'admin') {
-            window.location.href = 'admin-dashboard.html';
-        } else {
-            alert('Invalid admin username or password.');
-        }
-    }
+  }
 });
